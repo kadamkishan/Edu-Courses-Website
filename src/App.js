a1db4f5858310b2a6b7796aba37384616abd1a3b
Kishan Kadam <Kishank46175@gmail.com>
2023-11-06 00:48:33 +0530

    final touch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar"
 import Filter from "./components/Filter"
-import { apiUrl,filterData } from "./data";
-import Card from "./components/Card"
 import Spinner from "./components/Spineer"
 import Cards from "./components/Cards";
+import { apiUrl,filterData } from "./data";
 
 import {toast} from "react-toastify"
 
 const App = () => {
   const [courses,setCourses]=useState(null);
   const [loading,setLoading]=useState(true);
+  const [category,setCategory]=useState(filterData[0].title);
 
   async function fetchData(){
     setLoading(true);
@@ -33,22 +33,27 @@ const App = () => {
   
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-bgDark2">
       <div>
         <Navbar/>
       </div>
 
-      <div>
-        <Filter filterData={filterData}/>
+      <div className="bg-bgDark2">
+        <div>
+          <Filter filterData={filterData}
+                category={category}
+                setCategory={setCategory}
+            />
+        </div>
+
+        <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center 
+        min-h-[50vh]">
+          {
+            loading ?(<Spinner/>):(<Cards courses={courses} category={category} />)
+          }
+        </div>
       </div>
 
-      <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center 
-      min-h-[50vh]">
-        {
-          loading ?(<Spinner/>):(<Cards courses={courses} />)
-        }
-      </div>
-      
     </div>
   );
 
